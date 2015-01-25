ef930d63baea4d2f480ad79bb502ad0ccc71b684
Arthur Peters <amp@singingwizard.org>
2015-01-24 20:36:23 -0600

    Add sound support. with a simple ping sound for the impact of bubbles with the walls.

---
+++ UnityProject/Assets/Scripts/Particles/ParticleSound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSound : MonoBehaviour {
+	public AudioSource source;
+	void Start () {
+		// source = GameObject.Find ("ParticleHitAudio").GetComponent<AudioSource> ();
+	}
+
+	void OnParticleCollision(GameObject obj) {
+		(Object.Instantiate(source) as AudioSource).Play ();
+	}
+}
