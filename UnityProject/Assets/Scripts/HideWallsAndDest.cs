bc115a1e2c69a623907a205f0aea35ec091ea1b0
Arthur Peters <amp@singingwizard.org>
2015-01-24 18:49:03 -0600

    Make levels and prefabs for levels. Add script to hide walls and dest on load.

---
+++ UnityProject/Assets/Scripts/HideWallsAndDest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideWallsAndDest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    HideTag("Wall");
+            HideTag("Destination");
+	}
+
+        private void HideTag(string tag) {
+             GameObject[] objs = GameObject.FindGameObjectsWithTag(tag);
+             foreach ( GameObject o in objs ) {
+                 o.renderer.enabled = false;
+             }
+        }
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
