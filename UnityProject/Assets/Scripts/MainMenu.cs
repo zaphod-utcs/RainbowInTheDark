2eac68b038f152e17daf40a500cc5be9adab8860
Jason Brown <jbnetaccounts@yahoo.com>
2015-01-24 23:13:27 -0600

    add main menu

---
+++ UnityProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	#region Fields
+	const int buttonWidth = 355;
+	const int buttonHeight = 75;
+	Rect buttonRect;
+	Rect startButtonRect;
+	Rect controlsButtonRect;
+	Rect backButtonRect;
+	#endregion
+
+	void Start() {
+		startButtonRect = new Rect(740,780,buttonWidth,buttonHeight);
+		controlsButtonRect = new Rect(780,780,buttonWidth,buttonHeight);
+		backButtonRect = new Rect(795,867,buttonWidth,buttonHeight);
+	}
+	void OnGUI()
+	{
+
+		// Draw a button to start the game
+		if(GUI.Button(startButtonRect,""))
+		{
+			Debug.Log ("Start clicked");
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			//Application.LoadLevel("Stage1");
+		}
+		
+		if(GUI.Button(controlsButtonRect,""))
+		{
+			Debug.Log ("Controls clicked");
+			// On Click, load the first level.
+			// "Stage1" is the name of the first scene we created.
+			//Application.LoadLevel("Stage1");
+		}
+	}
+}
