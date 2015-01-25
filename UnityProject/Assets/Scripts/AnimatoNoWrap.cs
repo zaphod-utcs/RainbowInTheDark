687cf2c7ac80066a5af610f789461c9474bd4a42
Arthur Peters <amp@singingwizard.org>
2015-01-25 12:44:10 -0600

    Add fade for level name

---
+++ UnityProject/Assets/Scripts/AnimatoNoWrap.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatoNoWrap : MonoBehaviour {
+	private SpriteRenderer sprite;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer> ();
+				startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float t = Time.time - startTime;
+		if (t > 1) {
+			sprite.color = new Color (1, 1, 1, 1 - (t - 1));
+		}
+	}
+}
